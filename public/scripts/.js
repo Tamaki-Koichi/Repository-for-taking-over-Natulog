59a9cdb195e2533cd29af116b7fed132f79f6415
Tamaki-Koichi <ittiy.emotion.rocks@gmail.com>
2022-01-11 10:32:20 +0900

    add new file

---
+++ public/scripts/.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunknatulog"] = self["webpackChunknatulog"] || []).push([["src_index1_js"],{
+
+/***/ "./src/index1.js":
+/*!***********************!*\
+  !*** ./src/index1.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Hello\": () => (/* binding */ Hello)\n/* harmony export */ });\n// import aisatsu from \"./sub.js\";\r\n// aisatsu(\"こんにちは、うぇぶぱっく。\");\r\n\r\nfunction Hello(){\r\n  const mari = \"Hello World\";\r\n}\r\n\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXgxLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL25hdHVsb2cvLi9zcmMvaW5kZXgxLmpzPzg0ZTUiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IGFpc2F0c3UgZnJvbSBcIi4vc3ViLmpzXCI7XHJcbi8vIGFpc2F0c3UoXCLjgZPjgpPjgavjgaHjga/jgIHjgYbjgYfjgbbjgbHjgaPjgY/jgIJcIik7XHJcblxyXG5mdW5jdGlvbiBIZWxsbygpe1xyXG4gIGNvbnN0IG1hcmkgPSBcIkhlbGxvIFdvcmxkXCI7XHJcbn1cclxuXHJcbmV4cG9ydCB7SGVsbG99OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index1.js\n");
+
+/***/ })
+
+}]);
